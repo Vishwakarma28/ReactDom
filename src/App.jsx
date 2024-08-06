a37c7e6954a4654de9d2ce69ded3c09858fed9f0
Vishwakarma28 <Amit9301736578@gmail.com>
2024-08-06 19:08:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='bg-green-600 p-4'>helo</h1>
+    </>
+  )
+}
+
+export default App
